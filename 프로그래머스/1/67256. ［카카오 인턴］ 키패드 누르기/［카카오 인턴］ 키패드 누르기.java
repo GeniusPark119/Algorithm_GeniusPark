5b6b5d2a4e08bfd37172822e49f9403c6f5a75a2
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-11-12 21:20:26 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.55 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,92 +1,41 @@
 class Solution {
-    public int[] two ={3,1,0,1,2,1,2,3,2,3,4}; // 2까지 각 index 숫자로부터의 거리 10번은 *#
-    public int[] five ={2,2,1,2,1,0,1,2,1,2,3}; // 5까지 각 index 숫자로부터의 거리 10번은 *#
-    public int[] eight ={1,3,2,3,2,1,2,1,0,1,2}; // 8까지 각 index 숫자로부터의 거리 10번은 *#
-    public int[] zero ={0,4,3,4,3,2,3,2,1,2,1}; // 0까지 각 index 숫자로부터의 거리 10번은 *#
+    //0부터 9까지 좌표
+    int[][] numpadPos = {
+        {3,1},
+        {0,0},{0,1},{0,2},
+        {1,0},{1,1},{1,2},
+        {2,0},{2,1},{2,2}
+    };
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
     public String solution(int[] numbers, String hand) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-        boolean right = true;
-        if(hand.equals("left")) right = false;
-        int nowR = 10;//#
-        int nowL = 10;//*
-        for(int x : numbers){
-            if(x==1||x==4||x==7){
-                nowL = x;
-                sb.append("L");
-            }
-            else if(x==3||x==6||x==9){
-                nowR = x;
-                sb.append("R");
-            }else{
-                if(x==2){
-                    if(two[nowL]>two[nowR]){
-                        nowR = x;
-                        sb.append("R");
-                    }else if(two[nowL]<two[nowR]){
-                        nowL = x;
-                        sb.append("L");
-                    }else{
-                        if(right){
-                            nowR = x;
-                            sb.append("R");
-                        }else{
-                            nowL = x;
-                            sb.append("L");
-                        }
-                    }
-                }if(x==5){
-                    if(five[nowL]>five[nowR]){
-                        nowR = x;
-                        sb.append("R");
-                    }else if(five[nowL]<five[nowR]){
-                        nowL = x;
-                        sb.append("L");
-                    }else{
-                        if(right){
-                            nowR = x;
-                            sb.append("R");
-                        }else{
-                            nowL = x;
-                            sb.append("L");
-                        }
-                    }
-                }if(x==8){
-                    if(eight[nowL]>eight[nowR]){
-                        nowR = x;
-                        sb.append("R");
-                    }else if(eight[nowL]<eight[nowR]){
-                        nowL = x;
-                        sb.append("L");
-                    }else{
-                        if(right){
-                            nowR = x;
-                            sb.append("R");
-                        }else{
-                            nowL = x;
-                            sb.append("L");
-                        }
-                    }
-                }if(x==0){
-                    if(zero[nowL]>zero[nowR]){
-                        nowR = x;
-                        sb.append("R");
-                    }else if(zero[nowL]<zero[nowR]){
-                        nowL = x;
-                        sb.append("L");
-                    }else{
-                        if(right){
-                            nowR = x;
-                            sb.append("R");
-                        }else{
-                            nowL = x;
-                            sb.append("L");
-                        }
-                    }
-                }
-            }
+        this.hand = hand.equals("left")? "L" : "R";
+        
+        for(int num : numbers){
+            String Umjii = pushNumber(num);
+            sb.append(Umjii);
+            if(Umjii.equals("L")) leftPos = numpadPos[num];
+            else rightPos = numpadPos[num];
         }
         answer = sb.toString();
         return answer;
+    }
+    private String pushNumber(int num){
+        if(num==1||num==4||num==7){
+            return "L";
+        }
+        if(num==3||num==6||num==9){
+            return "R";
+        }
+        if(getDist(leftPos,num)<getDist(rightPos,num)) return "L";
+        if(getDist(leftPos,num)>getDist(rightPos,num)) return "R";
+        
+        return hand;
+    }
+    private int getDist(int[] pos,int num){
+        return Math.abs(pos[0]-numpadPos[num][0])+Math.abs(pos[1]-numpadPos[num][1]);
     }
 }
