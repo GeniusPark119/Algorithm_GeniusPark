a05bc30219bb65264ccbe7dd6bee42b36736d087
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-11-14 15:14:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.10 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,44 +1,65 @@
 class Solution {
     public String solution(String new_id) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        //1단계
-        new_id = new_id.toLowerCase();
-        //2단계
-        for(int i=0;i<new_id.length();i++){
-            char c = new_id.charAt(i);
-            if(('a'<=c&&c<='z')||('0'<=c&&c<='9')||c=='-'||c=='_'||c=='.'){
-                sb.append(c);
-            }
-        }
-        answer = sb.toString();
-        // System.out.println(answer);
-        // 3단계
-        while(answer.contains(".."))
-              answer= answer.replace("..",".");
-        
-        // 4단계
-        while(answer.length()>0 && answer.charAt(0)=='.'){
-            answer = answer.substring(1,answer.length());
-        }
-        while(answer.length()>0 && answer.charAt(answer.length()-1)=='.'){
-            answer = answer.substring(0,answer.length()-1);
-        }
-         //5단계
-        if(answer.length()==0) answer = "a";
-       
-        //6단계
-        if(answer.length()>15) answer= answer.substring(0,15);
-        while(answer.length()>0 && answer.charAt(answer.length()-1)=='.'){
-            answer = answer.substring(0,answer.length()-1);
-        }
-        //7단계
-        if(answer.length()<3) {
-            while(answer.length()<3){
-                answer += answer.charAt(answer.length()-1);
-            }
-        }
-        
+        answer = new KAKAOID(new_id).
+                replaceToLowerCase().
+                filter().
+                toSingleDot().
+                noStartEndDot().
+                noBlank().
+                noGreaterThan15().
+                noLessThan3().
+                getResult();
+                        
         return answer;
+    }
+    
+    private static class KAKAOID{
+        private String s;
+        
+        KAKAOID(String s){
+            this.s = s;
+        }
+        
+        private KAKAOID replaceToLowerCase(){
+            s = s.toLowerCase();
+            return this;
+        }
+        
+        private KAKAOID filter(){
+            s = s.replaceAll("[^a-z0-9._-]","");
+            return this;
+        }
+        private KAKAOID toSingleDot(){
+            s = s.replaceAll("[.]{2,}",".");
+            return this;
+        }
+        private KAKAOID noStartEndDot(){
+            s = s.replaceAll("^[.]|[.]$","");
+            return this;
+        }
+        private KAKAOID noBlank(){
+            if(s.isEmpty()) s = "a";
+            return this;
+        }
+        private KAKAOID noGreaterThan15(){
+            if(s.length()>15) {
+                s = s.substring(0,15);
+                s = s.replaceAll("[.]$","");
+            }
+            return this;
+        }
+        private KAKAOID noLessThan3(){
+            StringBuilder sb = new StringBuilder(s);
+            while(sb.length()<3){
+                sb.append(sb.charAt(sb.length()-1));
+            }
+            s = sb.toString();
+            return this;
+            
+        }
+        private String getResult(){
+            return s;
+        }
     }
 }
