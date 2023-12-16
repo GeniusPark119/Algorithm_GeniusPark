f5f13d5d425dc489cf3202fbbe2172b0d47cbe6f
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-12-16 20:33:19 +0900

    [level 2] Title: 귤 고르기, Time: 85.58 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,47 +1,30 @@
 //231216 18:50 ~ 19:30
 
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public Map<Integer,Integer> map = new HashMap<>();
     public int solution(int k, int[] tangerine) {
+        
         int answer = 0;
         int minus = tangerine.length - k;
+        
         for(int x : tangerine){
             map.put(x,map.getOrDefault(x,0)+1);
         }
-        int[] size = new int[map.size()];
-        int[] cnt = new int[map.size()];
-        int idx = 0;
-        for(Entry<Integer,Integer> entry : map.entrySet()){
-            size[idx] = entry.getKey();
-            cnt[idx] = entry.getValue();
-            idx++;
-        }
-
-        int tmp = 0;
-        while(minus>0){
-            int min = Integer.MAX_VALUE;
-            int min_idx = -1;
-            
-            for(int i=0;i<cnt.length;i++){
-                if(cnt[i]!=-1 && min>cnt[i]) {
-                    min = cnt[i];
-                    min_idx = i;
-                }
-            }
-            if(min<=minus)
-                cnt[min_idx] = -1;
-            else
-                cnt[min_idx] -= minus;
-            
-            minus -= min;
-        }
         
-        for(int x : cnt){
-            if(x!=-1) answer++;
+        List<Integer> list = new ArrayList(map.keySet());
+        list.sort((o1,o2)-> map.get(o2)-map.get(o1));
+        
+        for(Integer key:list){
+            k-=map.get(key);
+            answer++;
+            if(k<=0){
+                break;
+            }
         }
         return answer;
     }
