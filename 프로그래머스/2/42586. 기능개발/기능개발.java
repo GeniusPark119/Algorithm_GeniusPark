2b373fff35d07da7c90786fdcb1c49165c6354b0
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-12-20 15:01:19 +0900

    [level 2] Title: 기능개발, Time: 17.99 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,42 +1,56 @@
-
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 class Solution {
-
-    public List<Integer> list = new ArrayList<>();
-    public int[] checkProgresses;
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        checkProgresses = new int[progresses.length];
-        int next_idx = 0;
-        while(next_idx<progresses.length){
-            
-            for(int i=next_idx;i<progresses.length;i++){
-                progresses[i] += speeds[i];
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
-            
-            if(progresses[next_idx]>=100){
-                int cnt_completed = 0;
-                for(int i=next_idx;i<progresses.length;i++){
-                    if(progresses[i]>=100) {
-                        cnt_completed++;
-                        next_idx++;
-                    }else{
-                        break;
-                    }
-                }
-                list.add(cnt_completed);
-            }
-            
+            dayOfend[day]++;
         }
-        
-        answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
-        }
-        
-        
-        return answer;
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
+// import java.util.List;
+// import java.util.ArrayList;
+
+// class Solution {
+
+//     public List<Integer> list = new ArrayList<>();
+//     public int[] checkProgresses;
+//     public int[] solution(int[] progresses, int[] speeds) {
+//         int[] answer = {};
+//         checkProgresses = new int[progresses.length];
+//         int next_idx = 0;
+//         while(next_idx<progresses.length){
+            
+//             for(int i=next_idx;i<progresses.length;i++){
+//                 progresses[i] += speeds[i];
+//             }
+            
+//             if(progresses[next_idx]>=100){
+//                 int cnt_completed = 0;
+//                 for(int i=next_idx;i<progresses.length;i++){
+//                     if(progresses[i]>=100) {
+//                         cnt_completed++;
+//                         next_idx++;
+//                     }else{
+//                         break;
+//                     }
+//                 }
+//                 list.add(cnt_completed);
+//             }
+            
+//         }
+        
+//         answer = new int[list.size()];
+//         for(int i=0;i<list.size();i++){
+//             answer[i] = list.get(i);
+//         }
+        
+        
+//         return answer;
+//     }
+// }
