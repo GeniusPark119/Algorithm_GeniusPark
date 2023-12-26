4bf96437bc1c3645ed63a37efc15a60b076ff67b
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-12-26 14:07:07 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int[][] dungeons;
+    private boolean[] visited;
+    private int depth = 0;
+    private int max_dep = 0;
+    public int solution(int k, int[][] dungeons) {
+        int answer = 0;
+        this.dungeons = dungeons;
+        visited = new boolean[dungeons.length];
+        dfs(0,k);
+        answer = max_dep;
+        return answer;
+    }
+    private void dfs(int depth,int fatigue){
+        for(int i=0;i<dungeons.length;i++){
+            if(visited[i] || fatigue<dungeons[i][0]){
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth+1,fatigue-dungeons[i][1]);
+            visited[i] = false;
+        }
+        max_dep = Math.max(max_dep,depth);
+        
+    }
+}
