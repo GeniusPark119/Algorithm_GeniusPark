6e8896d11cae16f404bfb7d9784266e667de671a
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2023-12-27 14:45:03 +0900

    [level 2] Title: 타겟 넘버, Time: 2.32 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,7 +4,6 @@ class Solution {
     int[] now;
     int target;
     boolean[] visited; // 양수 방문처리
-    boolean[] visited_minus; // 음수 방문처리
     int answer = 0;
     int sum = 0; // 현재 숫자
     
@@ -12,7 +11,7 @@ class Solution {
         this.numbers = numbers;
         this.target = target;
         visited = new boolean[numbers.length];
-        visited_minus = new boolean[numbers.length];
+        // visited_minus = new boolean[numbers.length];
         now = new int[numbers.length];
         dfs();
         
@@ -22,17 +21,15 @@ class Solution {
     public void dfs(){
         
         for(int i=0;i<numbers.length;i++){
-            if(visited[i] || visited_minus[i]) continue;
+            if(visited[i]) continue;
             
             now[i] = numbers[i];
             visited[i] = true;
             dfs();
-            visited[i] = false;
-
+            
             now[i] = numbers[i]*(-1);
-            visited_minus[i] = true;
             dfs();
-            visited_minus[i] = false;
+            visited[i] = false;
             return;
         }
         
