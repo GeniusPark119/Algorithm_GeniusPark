1fbc408c4a9253a560b6c700ef4096c2d02bde80
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-02 19:48:58 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.26 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,23 +1,55 @@
-// 초과한 시간이 단위 시간으로 나누어 떨어지지 않으면, 올림합니다.
-
-// fee 배열 :  기본시간(분) / 기본요금 / 단위시간(분) / 단위요금
-// records 배열  :   00:00  /  차량번호4자리  / IN or OUT 
-
-
-// 1. 차량별 누적시간을 구해야한다. -> 00:00 끼리 몇분텀인지 계산해야함, 출차없는 차량에 12:59 반영해야함
-// 2. 주차요금을 구해야한다. 기본요금으로 끝나는지 ? 아니면 +  누적/단위시간의 올림 * 단위요금 
-// 3. 차량번호의 오름차순대로 차량요금을 정렬한다.
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.Arrays;
 
 class Solution {
+    Map<String,Integer> map_time = new TreeMap<>(); // 주차 시간을 저장해 놓는 map_time
+    
+    static int basicTime,basicFee,standardTime,standardFee;
+    
     public int[] solution(int[] fees, String[] records) {
         int[] answer = {};
-        getTime("01:01","02:02");
+        
+        basicTime = fees[0];
+        basicFee = fees[1];
+        standardTime = fees[2];
+        standardFee = fees[3];
+        
+        for(String x : records){
+            String[] record = x.split(" ");
+            int inOut = record[2].equals("IN")? -1 : 1;
+            map_time.put(record[1],map_time.getOrDefault(record[1],0)+getTime(record[0])*inOut);
+        }
+        
+        for(Entry<String,Integer> entry : map_time.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if(value<=0)
+            map_time.put(key,value+getTime("23:59"));
+                
+        }
+
+        answer = new int[map_time.size()];
+        int idx = 0;
+        for(Entry<String,Integer> entry : map_time.entrySet()){
+            answer[idx++] = getFee(entry.getValue());
+        }
+        
         return answer;
     }
-    public int getTime(String start,String end){
-        String[] time1 = start.split(":");
-        String[] time2 = end.split(":");
-        System.out.println(Integer.valueOf(time1[0]));
-        return 3;
+    
+    public int getTime(String StrTime){ // 00:00 형태를 분으로 계산하는 메서드
+        String[] time = StrTime.split(":");     
+        return Integer.valueOf(time[0])*60+Integer.valueOf(time[1]);   
+    }
+    
+    public int getFee(int time){ // 요금 구하는 메서드
+        if(time<=basicTime) 
+            return basicFee;
+        else{
+            return basicFee + (int)(Math.ceil((double)(time - basicTime)/standardTime))
+                *standardFee;
+        }
     }
 }
