9af519d1398daf132d5486b0cb1d9545cb05fb11
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-09 17:44:48 +0900

    [level 2] Title: 가장 큰 수, Time: 2.18 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strNums = new String[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+            strNums[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strNums,(o1,o2)->(o2+o1).compareTo(o1+o2));
+        if(strNums[0].equals("0")) return "0";
+        StringBuilder sb = new StringBuilder();
+        for(String s : strNums){
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
