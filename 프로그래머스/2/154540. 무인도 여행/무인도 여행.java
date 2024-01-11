a5608a0e1a6198f27bb6fbbad4ea2f7b3ed0c5f3
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-11 15:04:39 +0900

    [level 2] Title: 무인도 여행, Time: 0.60 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -4,49 +4,35 @@ import java.util.ArrayList;
 class Solution {
     
     boolean[][] visited;
-    int[][] board_maps;
+    int[][] maps;
     int[] dx = {0,0,-1,1};
     int[] dy = {-1,1,0,0};
     List<Integer> list = new ArrayList<>();
     
     public int[] solution(String[] maps) {
         int[] answer = {};
-        board_maps = new int[maps.length][maps[0].length()];
+        this.maps = new String[maps.length][maps[0].length()];
         visited = new boolean[maps.length][maps[0].length()];
-        // maps 배열을 int 배열로 하고 x인 부분을 0으로 함과 동시에 visited true로 체크한다.
         for(int i=0;i<maps.length;i++){
             for(int j=0;j<maps[0].length();j++){
                 if(maps[i].charAt(j)=='X'){
                     visited[i][j] = true;
-                    board_maps[i][j] = 0;
+                    this.maps[i][j] = 0;
                 }   
                 else 
-                    board_maps[i][j] = maps[i].charAt(j)-'0';
+                    this.maps[i][j] = maps[i].charAt(j)-'0';
             }
-        }
-        
-        for(int i=0;i<board_maps.length;i++){
-            for(int j=0;j<board_maps[0].length;j++){
-                System.out.print(board_maps[i][j]);
-            }
-            System.out.println();
         }
         
         findFood();
-        
-        // list -> answer
-        if(list.isEmpty()) list.add(-1);
-        answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
-        }
+        System.out.println(list);
+        if(list.isEmpty()) answer = 0;
         return answer;
     }
-    
     public void findFood(){
-        for(int i=0;i<board_maps.length;i++){
-            for(int j=0;j<board_maps.length;j++){
-                if(board_maps[i][j]!=0 && !visited[i][j]){
+        for(int i=0;i<maps.length;i++){
+            for(int j=0;j<maps[0].length;j++){
+                if(maps[i][j]!=0 && !visited[i][j]){
                    list.add(sumOfFood(i,j));
                 }
             }
@@ -54,7 +40,7 @@ class Solution {
     }
     public int sumOfFood(int x,int y){
         int answer = 0;
-        if(board_maps[x][y] == 0) return 0;
+        if(maps[x][y] == 0) return 0;
         
         for(int i=0;i<4;i++){
             int nextX = x + dx[i];
@@ -62,13 +48,13 @@ class Solution {
             
             if(inRange(nextX,nextY) && !visited[nextX][nextY]){
                 visited[nextX][nextY] = true;
-                answer += Integer.valueOf(board_maps[x][y]) + sumOfFood(nextX,nextY);
+                answer += Integer.valueOf(maps[x][y]) + sumOfFood(nextX,nextY);
             }
         }
         
         return answer;
     }
     public boolean inRange(int x,int y){
-        return x>=0 && x<board_maps.length && y>=0 && y<board_maps[0].length;
+        return x>=0 && x<maps.length && y>=0 && y<maps.length;
     }
 }
