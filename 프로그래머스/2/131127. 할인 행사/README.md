cbc734bc1ec06a924fb06dd680930fcb3596b596
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-17 16:07:15 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 0.03 ms
+메모리: 76.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 19:12:34
+2024년 1월 3일 16:7:12
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,35 +1,33 @@
-import java.util.Arrays;
+// 회원권 지불하면 10일동안 혜택을 받을 수 있음
+// 매일 한가지 제품을 할인하는 행사를 한다.
+// 내가 원하는거랑 10일동안 할인하는 제품이 정확히 일치해야 회원가입할거임
+
 import java.util.Map;
 import java.util.HashMap;
 
 class Solution {
+    Map<String , Integer> map = new HashMap<>();
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int dis_len = discount.length; // 전체 할인날의 수
-        int start = 0;
-        int end = 10;
-        int[] checkWant;
-        while(end<=dis_len){//전체 할인날을 10일로 끊는 경우의수
-            checkWant = new int[want.length];
-            String[] arr_ten = Arrays.copyOfRange(discount,start,end);
-            boolean isContains = true;
-            for(int i=0;i<want.length;i++){
-                for(String one : arr_ten) {
-                    if(want[i].equals(one)) checkWant[i]++;
-                }
+        for(int i=0; i < discount.length ; i++){
+            if(i>=10){
+                map.put(discount[i-10],map.getOrDefault(discount[i-10],1)-1);
+                if(map.get(discount[i-10])==0) map.remove(discount[i-10]);
             }
-            for(int i=0;i<want.length;i++){
-                if(number[i] !=checkWant[i]){
-                    isContains = false;
-                    break;
+            map.put(discount[i],map.getOrDefault(discount[i],0)+1);
+            // System.out.println(map);
+            if(map.size()==want.length){
+                boolean join = true;
+                for(int j=0;j<want.length;j++){
+                    if(!(map.containsKey(want[j]) && map.get(want[j])==number[j])){
+                        join = false;
+                        break;
+                    }
+                        
                 }
+                if(join) answer++;
             }
-            start++;
-            end++;
-            if(!isContains) continue;
-            else answer++;
         }
-        
         return answer;
     }
 }
