3fec3d0a5e3eeaa12e6f0aa880fbeb0e1593c4dc
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-18 21:44:40 +0900

    [level 2] Title: 무인도 여행, Time: 0.52 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,78 +1,59 @@
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 
 class Solution {
     String[] maps;
     boolean[][] visited;
-    int[] dx = { -1,1,0,0}; // 상하좌우
+    int[] dx = { -1,1,0,0};
     int[] dy = {0,0,-1,1};
     List<Integer> list = new ArrayList<>();
     public int[] solution(String[] maps) {
         int[] answer = {};
-        this.maps = maps;
         visited = new boolean[maps.length][maps[0].length()];
-        //  map을 순회하면서 숫자가 나오면 해당 bfs를 진행해서 list에 더해준다.
+        this.maps = maps;
         for(int i=0;i<maps.length;i++){
             for(int j=0;j<maps[0].length();j++){
-                if(maps[i].charAt(j)!='X'&&!visited[i][j]) {
-                    list.add(bfs(new Pos(i,j,maps[i].charAt(j)-'0')));
+                if(!visited[i][j] && maps[i].charAt(j)!='X'){
+                    visited[i][j] = true;
+                    
+                    list.add(dfs(new Pos(i,j,maps[i].charAt(j)-'0')));
                 }
             }
         }
-        //list가 비어있다면 -1 return 
-        if(list.isEmpty()) 
+        if(list.isEmpty())
             return new int[]{-1};
-        
-        // list 크기에 맞는 answer 배열을 만들고 정렬한다.
-        Collections.sort(list);
+        list.sort(Comparator.naturalOrder());
         answer = new int[list.size()];
         for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
+            answer[i]= list.get(i);
         }
-        
         return answer;
     }
-    public int bfs(Pos start){
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(start);
-        int food = 0;
+    public int dfs(Pos pos){
+        int food = pos.food;
         
-        visited[start.x][start.y] = true; // queue offer 뒤에는 무조건 방문철이
-        
-        while(!queue.isEmpty()){
-            Pos now = queue.poll();
+        for(int i=0;i<4;i++){
+            int nx = pos.x + dx[i];
+            int ny = pos.y + dy[i];
             
-            
-            food += now.food;
-            
-            for(int i=0;i<4;i++){
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                
-                if(0<= nx && nx < maps.length && 0<=ny && ny<maps[0].length() &&
-                  !visited[nx][ny] && maps[nx].charAt(ny)!='X'){
-                    // 사방탐색할때 방문체크 필수
-                    queue.offer(new Pos(nx,ny,maps[nx].charAt(ny)-'0'));
-                    // System.out.println(maps[nx].charAt(ny)-'0');
-                    visited[nx][ny] = true;// queue 넣을 때 방문철이
-                }
-            } 
-           
-            
+            if(0<=nx && nx < maps.length && 0<=ny && ny < maps[0].length() 
+               && !visited[nx][ny]
+              && maps[nx].charAt(ny) !='X'){
+                visited[nx][ny] = true;
+                food +=dfs(new Pos(nx,ny,maps[nx].charAt(ny)-'0'));
+            }
+
         }
-         
         return food;
-        
     }
-    class Pos{ // 위치와 그위치에 음식량
+    
+    class Pos{
         int x;
         int y;
         int food;
         
-        public Pos(int x , int y , int food){
+        public Pos(int x,int y,int food){
             this.x = x;
             this.y = y;
             this.food = food;
