78d85a9842a1acd2458c3fbda43ec28661e1f4f0
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-19 19:56:10 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,31 +1,23 @@
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-
-// 입지 않는 경우의수 1를 더한 값의 곱 -1 이다.
-// 예를 들어 얼굴-2 상의-3 하의-2 라면 각 입지않는 경우1를 더해 3*4*2를하고 다 안입는경우 1를뺀다
+import java.util.Map.Entry;
 
 class Solution {
-    public Map<String,Integer> map = new HashMap<>();
-    public List<Integer> list = new ArrayList<>();
+    Map<String,Integer> map;
     public int solution(String[][] clothes) {
         int answer = 1;
-        for(String[] cloth : clothes){
-            String name = cloth[0];
-            String kind = cloth[1];
-            map.put(kind,map.getOrDefault(kind,1)+1);
-        }
-        for(Entry<String,Integer> entry : map.entrySet()){
-            list.add(map.get(entry.getKey()));
+        map = new HashMap<>();
+        
+        for(String[] x : clothes){
+            String kind = x[1];
+            map.put(kind,map.getOrDefault(kind,0)+1);
         }
         
-        for( int x : list){
-            answer*=x;
+
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            answer *= entry.getValue()+1;
         }
         
         return answer-1;
     }
-
 }
