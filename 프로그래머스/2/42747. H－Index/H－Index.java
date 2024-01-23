c83c4060491fdedf42c7ab5bdf707398b0705b6a
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-23 15:47:18 +0900

    [level 2] Title: H-Index, Time: 0.39 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,25 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int max = -1;
-        for(int x : citations){
-            max = Math.max(max,x);
-        }
-        for(int i=max;i>=0;i--){
-            int cnt = 0;
-            for(int j=0;j<citations.length;j++){
-                if(citations[j]>= i){
-                    cnt++;
+        Arrays.sort(citations);
+        
+        int n = 0;
+        int cnt = 0;
+        
+        for(int i=0;i<citations.length;i++){
+            n = citations[i];
+            cnt = citations.length - i;
+            if(cnt<=n){
+                answer = cnt;
+                break;
             }
         }
-            // System.out.println("i "+i+"cnt "+cnt);
-            
-        if(cnt>=i) {
-            answer=i;
-            break;
-        }      
-    }
-        // if(answer==0) answer = citations.length;
         return answer;
     }
 }
