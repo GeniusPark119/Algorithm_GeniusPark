a157668a3d5a293ac6c9e4e713aaff4d5fd8b4d0
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-23 11:32:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,29 +1,27 @@
-//231216 16시 24분 ~ 16시 50분 (26분)
+// 11시 8분
 
 class Solution{
-    
-    public boolean isMeet(int a,int b){// 둘중하나가 홀수 , 짝수 일때 홀수+1 이 짝수가 되면 붙게된것
-        if(a%2==1 && b%2==0 && a+1 == b) return true; 
-        else if(a%2==0 && b%2 ==1 && b+1 == a ) return true;
-        return false;
-    }// 둘이 붙게된것인지 true false로 반환
-    
-    public int nextNum(int x) { // 해당 번호가 이기면 다음 번호가 어떻게 되는지 반환
-        if(x%2==0) return x/2;
-        else return (x+1)/2;
-    }
-    
-    public int solution(int n, int a, int b)
-    {
-        int answer = 0;
+    public int solution(int n, int a, int b){
+        int answer = 1;
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
         
         while(true){
-            answer++;
-            if(isMeet(a,b)) break;
-            a = nextNum(a);
-            b = nextNum(b);
-        }
+            if(min + 1 == max && min%2!=0)
+                break;
             
+            min = next(min);
+            max = next(max);
+            answer++;
+            
+        }
+
         return answer;
+    }
+    public int next(int num){ // 이긴 번호의 다음 번호
+        if(num%2==0) 
+            return num/2;
+        else 
+            return (num+1)/2;
     }
 }
