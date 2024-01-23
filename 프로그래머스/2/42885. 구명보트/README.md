fd41b63ea3599f6928b6d56c83c5f497ffcd1087
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-23 10:49:16 +0900

    [level 2] Title: 구명보트, Time: 22.24 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 54.3 MB, 시간: 19.96 ms
+메모리: 53.9 MB, 시간: 22.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 11:44:4
+2024년 1월 2일 10:49:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,51 +1,37 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-
-/// limit 이하인 사람들을 따로 모아서 정렬한다.
-// 가장작은사람이랑 큰사람을 연결지어본다. 그럼 안될때도 있다.
-// 50 50 70 80 작은수부터 더했을때 limit이 되는 수중 가장 큰수 제외
-
 class Solution {
-    public List<Integer> list = new ArrayList<>();
+    List<Integer> list;
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people); // 사람들의 몸무게를 오름차순으로 정렬한다.
-        
-        // people 배열에서 limit보다 작은수를 list에 담는다.
-        // limit과 같으면 answer를 1씩 증가한다.
+        list = new ArrayList<>();
+        Arrays.sort(people);
+
         for(int i=0;i<people.length;i++){
-            if(people[i]==limit) {
-                answer +=people.length - i;
+            if(people[i]==limit){
+                answer+=people.length-i;
                 break;
+                
             }
             list.add(people[i]);
         }
-        //list에 원소가 없을때까지
-        // 가장 작은 수와 가장 큰 수의 합이 limit 이하인지 확인하고
-        // 맞다면 answer 1를 늘리고 두 수를 list에서 삭제한다.
-        // 만약 limit을 넘어가면 answer를 1 늘리고 가장 큰수를 삭제한다.
-         while(list.size()>0){
-            if(list.size()==1) {
+        
+        while(!list.isEmpty()){
+            if(list.size()==1){
                 answer++;
                 break;
             }
-            int p1 = list.get(0);
-            int p2 = list.get(list.size()-1);
-        
-            if(p1+p2>limit){
-                answer+=1;
-                list.remove(list.size()-1);
-            }else{
-                answer+=1;
+            if(list.get(0)+list.get(list.size()-1)<=limit){
+                answer++;
                 list.remove(0);
                 list.remove(list.size()-1);
+            }else{
+                answer++;
+                list.remove(list.size()-1);
             }
-            
         }
+        
         return answer;
     }
-    
-  
-    
 }
