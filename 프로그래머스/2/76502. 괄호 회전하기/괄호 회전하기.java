fb0d585a2310f485031a34088b0b9e6c7afb8abb
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-23 13:43:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 15.24 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,32 +1,46 @@
-import java.util.Queue;
-import java.util.LinkedList;
 import java.util.Stack;
 
 class Solution {
-    public Queue<Character> queue = new LinkedList<>();
-    public Stack<Character> stack = new Stack<>();
+    Stack<Character> stack;
     public int solution(String s) {
         int answer = 0;
+        stack = new Stack<>();
+        
         for(int i=0;i<s.length();i++){
-            queue.offer(s.charAt(i));
-        }
-        int x = 0;
-        while(x<s.length()){
-            for(Character c : queue){
-                if(!stack.isEmpty() && ((c==')' && stack.peek()=='(')
-                    ||(c=='}' && stack.peek()=='{')
-                    ||(c==']' && stack.peek()=='['))){
-                    stack.pop();
-                }else 
-                    stack.push(c);
+            if(i==0){
+                if(check(s)) answer++;
+                continue;
             }
-            if(stack.isEmpty()) answer++;
-            stack.clear();
-            
-            char front = queue.poll();
-            queue.offer(front);
-            x++;
+            char tmp = s.charAt(0);
+            s = s.substring(1,s.length()) + tmp;
+            if(check(s)) {
+                // System.out.println("i "+i+" s "+s);
+                answer++;
+            }
         }
+
         return answer;
+    }
+    public boolean check(String s){
+        stack.clear();
+        for(int i=0;i<s.length();i++){
+            char tmp = s.charAt(i);
+            
+                if(!stack.isEmpty() && (
+                   (tmp==')' && stack.peek()=='(') ||
+                   (tmp=='}' && stack.peek()=='{')  ||
+                   (tmp==']' && stack.peek()=='[')
+                   )){
+                    stack.pop();
+                }else{
+                stack.push(tmp);
+                }
+            
+        }
+        if(stack.isEmpty()) 
+            return true;
+        else
+            return false;
+        
     }
 }
