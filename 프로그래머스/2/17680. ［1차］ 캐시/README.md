b5a7d9814f62e2a813859a4b0efa6a5f9ed350da
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-25 21:10:28 +0900

    [level 2] Title: [1차] 캐시, Time: 2.27 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 2.07 ms
+메모리: 73.7 MB, 시간: 2.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 15:45:54
+2024년 1월 4일 21:10:23
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,28 +1,33 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
-    public Queue<String> queue = new LinkedList<>();
+    public List<String> list; // list를 만들고 순차적으로 도시를 넣는다.
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
-        if(cacheSize==0){
-            return 5*cities.length;
-        }
-        for(String city : cities){
-            
-            city = city.toUpperCase();
-            
-            if(queue.contains(city)){
-                answer +=1;
-                queue.remove(city);
-            }else
+        list = new ArrayList<>();
+        for(String city : cities){ // cities를 순회한다.
+            city = city.toLowerCase(); // 모두 소문자로 바꾼다.
+            if(list.size()==0){ // 만약 list에 아무것도 없으면 5를 더하고 list에 추가한다.
                 answer+=5;
-            
-            if(queue.size()==cacheSize) 
-                queue.poll();
-            
-            queue.add(city);
+                if(cacheSize!=0)
+                    list.add(city);
+                continue;
+            }
+            if(list.contains(city)){ //만약 list에 city가 있다면
+                answer++; //1만 더하고
+                list.remove(city); // 해당 city를 빼고 다시 더한다.
+                list.add(city);
+            }else{
+                if(list.size()<cacheSize){ // 만약 city가 없다면 size가 적다면 추가하고 5를 내고
+                    list.add(city);
+                    answer+=5;
+                }else{ // size가 다 찾으면 첫번째를 빼고 지금걸 더한후 5를 더한다.
+                    list.remove(0);
+                    list.add(city);
+                    answer+=5;
+                }
+            }
         }
         return answer;
     }
