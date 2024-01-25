bd259bdf3228718f76fc81eff6c8a2cad08b7fc2
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-25 18:08:12 +0900

    [level 2] Title: 피보나치 수, Time: 12.33 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,20 @@
 class Solution {
+    public int[] pivos;
     public int solution(int n) {
         int answer = 0;
-        int f1 = 1;
-        int f2 = 1;
-        for(int i= 2;i<n;i++){
-            int f3 = (f1+f2)%1234567;
-            f1 = f2;
-            f2 = f3;
-        }
-        return f2;
+        pivos = new int[100001];
+        pivos[0] = 0;
+        pivos[1] = 1;
+        
+        answer = pivo(n);
+        return answer;
+    }
+    public int pivo(int num){
+        if(num==0) return 0;
+        if(pivos[num]!=0) return pivos[num];
+        pivos[num] = (pivo(num-1)+ pivo(num-2))%1234567;
+
+        return pivos[num];
+        
     }
 }
