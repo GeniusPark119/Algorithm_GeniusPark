96f9ee5a85a0ee7f771bcf989a467eff4d78627d
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-25 17:33:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,20 +1,27 @@
+// n 보다 큰 자연수
+// 2진수로 변환했을 때 1의 갯수가 같다.
+// 가장 작은수
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int cnt = 0;
-        String binN = Integer.toString(n,2);
-        for(int i=0;i<binN.length();i++){
-            if(binN.charAt(i)=='1') cnt++;
-        }
-        // System.out.println(binN);
+        int cntOne1 = findCntOne(n);
         while(true){
             n++;
-            int tmpCnt = 0;
-            String tmpN = Integer.toString(n,2);
-            for(int i=0;i<tmpN.length();i++){
-                if(tmpN.charAt(i)=='1') tmpCnt++;
+            int cntOne2 = findCntOne(n);
+            if(cntOne2==cntOne1) {
+                answer = n;
+                break;
             }
-            if(cnt==tmpCnt) return n;
         }
+        return answer;
+    }
+    public int findCntOne(int s){
+        int cntOne = 0;
+        String bin = Integer.toBinaryString(s);
+        for(int i=0;i<bin.length();i++){
+            if(bin.charAt(i)=='1') cntOne++;
+        }
+        return cntOne;
     }
 }
