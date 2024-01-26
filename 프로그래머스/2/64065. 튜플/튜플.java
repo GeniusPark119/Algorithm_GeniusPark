17a4c5873b571bd6971abe6529bee3ac1ffbe6c9
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-26 11:38:09 +0900

    [level 2] Title: 튜플, Time: 0.72 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,100 +1,27 @@
-import java.util.Arrays;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Arrays;
 
-class Solution{
-    public Set<String> set = new HashSet<>();
-    public int[] solution(String s){
-        int[] answer ={};
-        String[] arr = s.replaceAll("[{}]"," ").trim().split(" , ");
-        Arrays.sort(arr,(s1,s2)->s1.length()-s2.length());
+class Solution {
+    public Set<Integer> set;
+    public int[] solution(String s) {
+        int[] answer = {};
+        set = new HashSet<>();
+        String[] nums = s.replaceAll("[{}]"," ").trim().split(" , ");
+        Arrays.sort(nums,(o1,o2)->o1.length()-o2.length());
+        answer = new int[nums.length];
         
-        answer = new int[arr.length];
         int idx = 0;
-        for(String str : arr){
-            for(String num : str.split(",")){
-                if(set.add(num)) answer[idx++] = Integer.valueOf(num);
+        
+        for (String ss : nums){
+            String[] tmp = ss.split(",");
+            for(String t : tmp){
+                if(set.add(Integer.valueOf(t)))
+                    answer[idx++] = Integer.valueOf(t);
             }
-            
         }
         
-    
+        
         return answer;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //231218 15시 50분 ~ 21시 (3시간(식사시간포함))
-// import java.util.List;
-// import java.util.ArrayList;
-
-// class Solution {
-//     public List<List<Integer>> list = new ArrayList<>();
-//     public List<Integer> tmpList = new ArrayList<>();
-//     public int[] solution(String s) {
-//         int[] answer = {};
-//         s = s.replaceAll("},","-");
-//         // System.out.println(s);
-        
-//         StringBuilder sb = new StringBuilder();
-        
-//         for(int i=2;i<s.length()-1;i++){
-            
-//             char tmp = s.charAt(i);
-//             if('0'<=tmp&&tmp<='9') sb.append(tmp);
-//             if(tmp==','||tmp=='-'||tmp=='}'){
-//                 tmpList.add(Integer.valueOf(sb.toString()));
-//                 sb.setLength(0);
-//             }
-//             if(tmp=='{'){
-//                 list.add(tmpList);
-//                 tmpList = new ArrayList<>();
-//             }
-
-//             if(i==s.length()-2) list.add(tmpList);
-//         }
-        
-//         list.sort((o1,o2)->(o1.size()-o2.size()));
-//             // System.out.println(list);
-        
-//         answer = new int[list.size()];
-//         for(int i=0;i<list.size();i++){
-//             if(i==0){
-//                 answer[i] = list.get(i).get(i);
-//                 continue;
-//             }
-//             for(int j=0;j<list.get(i).size();j++){
-//                 if(!list.get(i-1).contains(list.get(i).get(j))){
-//                     answer[i] = list.get(i).get(j);
-//                 }
-//             }
-//         }
-        
-//         return answer;
-//     }
-// }
-
