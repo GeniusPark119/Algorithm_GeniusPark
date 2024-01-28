e5a41f2b65a53a0b216a5ef457f9c47a2bf5a841
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-28 13:05:08 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,27 +1,32 @@
+// 입장하기위한 피로도와 깎이는 피로도
+// 완전탐색을 한다면?
+// dep = 던전의 수일때 다시 되돌아간다.
+
 class Solution {
-    private int[][] dungeons;
-    private boolean[] visited;
-    private int depth = 0;
-    private int max_dep = 0;
+    static int[][] dungeons;
+    static boolean[] visited;
+    static int len;
+    static int cnt = 0;
     public int solution(int k, int[][] dungeons) {
         int answer = 0;
         this.dungeons = dungeons;
-        visited = new boolean[dungeons.length];
+        len = dungeons.length;
+        visited = new boolean[len];
         dfs(0,k);
-        answer = max_dep;
+        answer = cnt;
         return answer;
     }
-    private void dfs(int depth,int fatigue){
-        if(max_dep == dungeons.length) return;
-        for(int i=0;i<dungeons.length;i++){
-            if(visited[i] || fatigue<dungeons[i][0]){
+    public void dfs(int dep,int fatigue){
+        for(int i=0;i<len;i++){
+            if(visited[i] || dungeons[i][0]>fatigue){
                 continue;
             }
             visited[i] = true;
-            dfs(depth+1,fatigue-dungeons[i][1]);
+            dfs(dep+1,fatigue-dungeons[i][1]);
             visited[i] = false;
         }
-        max_dep = Math.max(max_dep,depth); 
+        cnt = Math.max(cnt,dep);
+        
         
     }
 }
