987a2e8cb11f907a909e3eac1875454fd03b0e8c
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-28 23:26:55 +0900

    [level 2] Title: 땅따먹기, Time: 40.78 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,19 +1,29 @@
-// 두번째 행부터 각 행마다 열별로 최대값을 찾는다. 
-// 예) 이번 행의 1열이라면 이전행 2~4열중의 최대값을 더한다. 
-// 이전 행과 이번행의 합이 최대가 되는 값을 각 열마다 입력하고 마지막행까지 반복한다.
-// 마지막행에서 제일 큰 수가 답이다.
+//2행부터 N행까지 최대값을 구한다.
+
 class Solution {
     int solution(int[][] land) {
-        for(int i=1;i<land.length;i++){
-            land[i][0] += Math.max(Math.max(land[i-1][1],land[i-1][2]),land[i-1][3]);
-            land[i][1] += Math.max(Math.max(land[i-1][2],land[i-1][0]),land[i-1][3]);
-            land[i][2] += Math.max(Math.max(land[i-1][1],land[i-1][0]),land[i-1][3]);
-            land[i][3] += Math.max(Math.max(land[i-1][1],land[i-1][2]),land[i-1][0]);
+        int answer = 0;
+        int[][] map = new int[land.length][4];
+        
+        for(int i=0;i<map.length;i++){
+            if(i==0){
+                map[i][0] = land[0][0];
+                map[i][1] = land[0][1];
+                map[i][2] = land[0][2];
+                map[i][3] = land[0][3];
+                continue;
+            }
+            map[i][0] = land[i][0] + Math.max(Math.max(map[i-1][1],map[i-1][2]),map[i-1][3]);
+            map[i][1] = land[i][1] + Math.max(Math.max(map[i-1][0],map[i-1][2]),map[i-1][3]);
+            map[i][2] = land[i][2] + Math.max(Math.max(map[i-1][1],map[i-1][0]),map[i-1][3]);
+            map[i][3] = land[i][3] + Math.max(Math.max(map[i-1][1],map[i-1][2]),map[i-1][0]);
+            
+            
+            if(i==map.length-1){
+                answer = Math.max(Math.max(map[i][0],map[i][1]),Math.max(map[i][2],map[i][3]));
+            }
         }
-        
-        int len = land.length-1;
-        
-        return Math.max(Math.max(land[len][1],land[len][2]),
-                        Math.max(land[len][3],land[len][0]));
+
+        return answer;
     }
 }
