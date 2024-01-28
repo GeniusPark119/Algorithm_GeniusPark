5279cb881d3510f0e2265edd18fbb54b41c7ef1e
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-28 21:43:03 +0900

    [level 2] Title: 타겟 넘버, Time: 0.33 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,46 +1,28 @@
 class Solution {
-    
-    int[] numbers;
-    int[] now;
+    int numbers[];
     int target;
-    boolean[] visited; // 양수 방문처리
-    int answer = 0;
-    int sum = 0; // 현재 숫자
-    
+    int answer;
     public int solution(int[] numbers, int target) {
+        answer = 0;
         this.numbers = numbers;
         this.target = target;
-        visited = new boolean[numbers.length];
-        // visited_minus = new boolean[numbers.length];
-        now = new int[numbers.length];
-        dfs();
+        dfs(0,0);
+        
         
         return answer;
     }
-    
-    public void dfs(){
-        
-        for(int i=0;i<numbers.length;i++){
-            if(visited[i]) continue;
-            
-            now[i] = numbers[i];
-            visited[i] = true;
-            dfs();
-            
-            now[i] = numbers[i]*(-1);
-            dfs();
-            visited[i] = false;
+    public void dfs(int dep,int sum){
+        if(dep==numbers.length) {
+            if(sum==target)
+                answer++;
             return;
         }
         
-        for(int x : now){
-            // System.out.print(x+" ");
-            sum +=x;
-        }
-        // System.out.println();
-        // System.out.println("sum "+sum);
-        if(sum == target) answer++;
-        sum=0;
+        int plus = sum +numbers[dep];
+        int minus = sum -numbers[dep];
+        
+        dfs(dep+1,plus);
+        dfs(dep+1,minus);
         
     }
 }
