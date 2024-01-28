d35898177caaa14c92c773cbc0285d687c15114c
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-28 16:44:44 +0900

    [level 2] Title: 모음 사전, Time: 3.84 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,59 +1,27 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    static String[] dictionary = {"A","E","I","O","U"};
     static List<String> list;
-    String word;
-    int answer;
+    static String[] words ={"A","E","I","O","U"};
     public int solution(String word) {
-        answer = 0;
+        int answer = 0;
         list = new ArrayList<>();
-        this.word = word;
         dfs("",0);
+        for(int i=0;i<list.size();i++){
+            // System.out.println(list.get(i));
+            if(list.get(i).equals(word)) return i;
+        }
         return answer;
     }
-    public void dfs(String str , int depth){
-        if(answer!=0) return;
-        
-        if(str.equals(word)) {
-            answer=list.size();
-            return;
-        }
-                              
+    
+    public void dfs(String str, int len){
         list.add(str);
-        if(depth==5) return;
+        if(len==5) return;
+        
         for(int i=0;i<5;i++){
-            dfs(str+dictionary[i],depth+1);
-        }     
+            dfs(str+words[i],len+1);
+        }
     }
 }
 
-
-// import java.util.ArrayList;
-// import java.util.List;
-// class Solution {
-//     static List<String> list;
-//     static String [] words = {"A", "E", "I", "O", "U"};
-//     public int solution(String word) {
-//         int answer = 0;
-//         list = new ArrayList<>();
-//         dfs("", 0);
-//         int size = list.size();
-//         for (int i = 0; i < size; i++) {
-//             if (list.get(i).equals(word)) {
-//                 answer = i;
-//                 break;
-//             }
-//         }
-//         return answer;
-//     }
-
-//     static void dfs(String str, int len) {
-//         list.add(str);
-//         if (len == 5) return;
-//         for (int i = 0; i < 5; i++) {
-//             dfs(str + words[i], len + 1);
-//         }
-//     }
-// }
