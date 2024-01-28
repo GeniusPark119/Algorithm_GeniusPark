40da4e49849a3fc899e5259e5b55d01a1358c559
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-28 15:23:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.60 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,62 +1,59 @@
 import java.util.Queue;
 import java.util.LinkedList;
-// import java.util.
 
 class Solution {
-    
-    class Pointer{
-        int v;
-        int h;
-        int time;
-        
-        public Pointer(int v,int h,int time){
-            this.v = v;
-            this.h = h;
-            this.time = time;
-        }
-        
-    }
-    int[][] maps;
-    boolean[][] visited;
-    Queue<Pointer> queue;
-    int[][] delta = {{-1,1,0,0},{0,0,-1,1}}; // dv와 dh 상하좌우
-    int len_v,len_h;
-    
+    static int[][] maps;
+    static boolean[][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     public int solution(int[][] maps) {
         int answer = 0;
-        
         this.maps = maps;
-        queue = new LinkedList<>();
-        len_v = maps.length;
-        len_h = maps[0].length;
-        visited = new boolean[len_v][len_h];
-
-        queue.offer(new Pointer(0,0,1));
-        visited[0][0] = true;
-        answer = bfs();
+        visited = new boolean[maps.length][maps[0].length];
+        answer = bfs(0,0);
         return answer;
     }
-    
-    public int bfs(){
+    public int bfs(int a,int b){
+        visited[a][b] = true;
+        Queue<Pointer> queue = new LinkedList<>();
+        queue.add(new Pointer(a,b,1));
+        
         while(!queue.isEmpty()){
-            Pointer p = queue.poll();
+            Pointer point = queue.poll();
+            int x = point.x;
+            int y = point.y;
+            int dep = point.dep;
             
-            if(p.v == len_v-1 && p.h == len_h-1) return p.time;
+            if(x == maps.length-1 && y == maps[0].length-1)
+                return dep;
             
             for(int i=0;i<4;i++){
-                int nv = p.v + delta[0][i];
-                int nh = p.h + delta[1][i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(isin(nv,nh) && !visited[nv][nh] && maps[nv][nh]==1){
-                    queue.offer(new Pointer(nv,nh,p.time+1));
-                    visited[nv][nh] = true;
+                if(0<= nx && nx <maps.length && 0<=ny && ny<maps[0].length 
+                   && maps[nx][ny] == 1
+                   && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    queue.offer(new Pointer(nx,ny,dep+1));
                 }
-            }      
+            }
+            
         }
+        
         return -1;
+        
     }
     
-    public boolean isin(int nv,int nh){
-        return 0<=nv && nv < len_v && 0<=nh && nh < len_h;
+    class Pointer{
+        int x = 0;
+        int y = 0;
+        int dep = 0;
+        
+        public Pointer(int x,int y,int dep){
+            this.x = x;
+            this.y = y;
+            this.dep =dep;
+        }
     }
 }
