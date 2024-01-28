2f8d95736a283e01460b6910da0708adf48d603c
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-29 03:09:14 +0900

    [level 2] Title: 소수 찾기, Time: 11.91 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,50 +2,50 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.Arrays;
 
-class Solution { // 2024/1/9 14:50 시작
-    static int depth = 0;
-    static boolean[] visited;
-    static int[] nums;
-    static StringBuilder sb = new StringBuilder();
-    static Set<Integer> set = new HashSet<>();
+class Solution {
+    int dep = 0;
+    boolean[] visited;
+    int[] nums;
+    StringBuilder sb;
+    Set<Integer> set;
     public int solution(String numbers) {
         int answer = 0;
         visited = new boolean[numbers.length()];
-
+        
         nums = new int[numbers.length()];
+        sb = new StringBuilder();
+        set = new HashSet<>();
+        
         for(int i=0;i<numbers.length();i++){
             nums[i] = numbers.charAt(i)-'0';
         }
-        
         Arrays.sort(nums);
         
         makeNum(0,"");
-        
         // System.out.println(set);
         
         for(int s : set){
-            if(isSosu(s)) answer++;
+            if(isSosu(s))
+                answer++;
         }
         
         return answer;
     }
-    //숫자조합 메서드
-    public void makeNum(int depth,String str){
-        if(depth > nums.length ) {
+    public void makeNum(int dep,String str){
+        if(dep>nums.length)
             return;
-        }
         for(int i=0;i<nums.length;i++){
-            if(!visited[i]) {
-                visited[i] = true; 
+            if(!visited[i]){
+                visited[i] = true;
                 set.add(Integer.valueOf(str+nums[i]));
-                makeNum(depth++,str+nums[i]);
-                visited[i] = false; 
-                depth--;
-                }
+                makeNum(dep++,str+nums[i]);
+                visited[i] = false;
+                dep--;
+            }
         }
     }
-    //소수판별 메서드
     public boolean isSosu(int num){
+        
         if(num<=1) return false;
         for(int i=2;i<=(int)Math.sqrt(num);i++){
             if(num%i==0) return false;
