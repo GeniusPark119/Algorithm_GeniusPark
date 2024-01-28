a31acc9833222e462022a1a4faa119e41973c7ad
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-01-29 01:15:12 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.52 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,33 +1,25 @@
-// k 진수로 바꾸는 메서드
-// 소수 판별하는 메서드
-// 0이 들어가지 않는 수 골라내는 메서드
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String[] numbers = change(n,k).split("0");
+        String[] numbers = Integer.toString(n,k).split("0");
+        System.out.println(Integer.toString(n,k));
+        
         for(String x : numbers){
-            
             if(x.length()==0) continue;
             if(isSosu(Double.valueOf(x))) answer++;
         }
-        
         return answer;
     }
-    public String change(int num,int k){// k진수로 바꾸는 메서드
-        if(k==10) return num+"";
-        StringBuilder sb = new StringBuilder();
-        while(num!=0){
-            sb.append(num%k);
-            num/=k;
-        }
-        return sb.reverse().toString();
-    }
+    // public String change(int num,int k)
     
-    public boolean isSosu(Double num){ // 소수판별
+    public boolean isSosu(Double num){
         if(num==1) return false;
         for(int i=2;i<=(int)Math.sqrt(num);i++){
-            if(num%i==0) return false;
+            if(num%i==0)
+                return false;
         }
+        System.out.println(num);
         return true;
     }
+        
 }
