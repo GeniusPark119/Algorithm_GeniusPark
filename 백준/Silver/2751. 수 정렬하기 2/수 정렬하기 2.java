659443b87c508fe50f28f4266a0df22da24bdb2e
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-02-13 18:57:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2804 ms, Memory: 335384 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 //직각삼각형
@@ -7,19 +10,22 @@ import java.util.PriorityQueue;
 public class Main {
     static PriorityQueue<Integer> pq;
     static StringBuilder sb;
+    static List<Integer> list;
     public static void main(String[] args) {
         pq = new PriorityQueue<>();
+        list = new ArrayList<>();
         sb = new StringBuilder();
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         for(int i=0;i<n;i++){
-            pq.offer(sc.nextInt());
+            list.add(sc.nextInt());
         }
+        Collections.sort(list);
 
-        while(!pq.isEmpty()){
-            sb.append(pq.poll());
+        for(int i=0;i<list.size();i++){
+            sb.append(list.get(i));
             sb.append("\n");
         }
         System.out.println(sb.toString());
