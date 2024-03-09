e1e220065dfb2f0adc17ad47baa55ada5298f867
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-03-10 02:33:53 +0900

    [Bronze V] Title: A+B - 2, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+
+        System.out.print(a + b);
+
+    }
+
+
+}
