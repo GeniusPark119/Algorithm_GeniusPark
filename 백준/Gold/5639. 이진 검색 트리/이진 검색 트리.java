ed2f1beb371eb779fe3c87c225802ec461568f6b
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-03-14 21:06:21 +0900

    [Gold V] Title: 이진 검색 트리, Time: 648 ms, Memory: 31612 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        Node root = new Node(Integer.parseInt(br.readLine()));
+
+       while(true){
+           String str = br.readLine();
+           if(str == null || str.equals("")) break;
+           root.insert(Integer.parseInt(str));
+       }
+
+       postOrder(root);
+
+
+    }
+
+    public static void postOrder(Node node){
+        if(node==null) return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.mid);
+    }
+
+    static class Node{
+        int mid;
+        Node left,right;
+
+        Node(int mid){
+            this.mid = mid;
+        }
+        Node(int mid,Node left,Node right){
+            this.mid = mid;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int n){
+            if(n<this.mid){
+                if(this.left == null)
+                    this.left = new Node(n);
+                else
+                    this.left.insert(n);
+            }else{
+                if(this.right == null)
+                    this.right = new Node(n);
+                else
+                    this.right.insert(n);
+            }
+        }
+
+    }
+
+}
+
