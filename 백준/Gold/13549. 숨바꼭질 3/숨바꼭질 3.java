42a2f61316365c88d8c78bf36e1e5b19703b37be
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-04-17 03:02:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 18868 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -4,46 +4,43 @@ import java.util.*;
 
 public class Main {
 
-    private static int bfs(int n, int k) {
-        Queue<int[]> queue = new LinkedList<>(); // { 위치 , 시간 }
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 소정이
+        int K = Integer.parseInt(st.nextToken()); // 상빈이
+
+        Queue<int[]> queue = new LinkedList<>(); // { 소정위치 , 시간 }
         boolean[] visited = new boolean[100_001];
 
-        queue.offer(new int[]{n, 0});
-        visited[n] = true; // 이거 체크 안하면 시간 초과!! bfs할 때 꼭 생각하기 
+        queue.offer(new int[]{N, 0});
+        visited[N] = true; // 이거 체크 안하면 시간 초과!! bfs할 때 꼭 생각하기
 
         while (!queue.isEmpty()) {
             int[] arr = queue.poll();
             int point = arr[0];
             int time = arr[1];
 
-            if (point == k) // 동생이 있는 자리에 갔다면 return
-                return time;
+            if (point == K) {// 상빈이 있는 자리에 갔다면 return
+                System.out.print(time);
+                return;
+            }
 
-            if (0 <= point * 2 && point * 2 <= 100_000 && !visited[point * 2]) { // *2 자리로 갔을 때 범위내에 있다면 queue에 시간 추가 안하고 저장
+            // 이식이 제일 먼저 있어야 최소값이 저장되면서 visited 처리가 된다. 순서 바꾸면 틀림
+            if (point <= 50_000 && !visited[point * 2]) { // *2 자리로 갔을 때 범위내에 있다면 queue에 시간 추가 안하고 저장
                 visited[point * 2] = true;
                 queue.offer(new int[]{point * 2, time});
             }
-            if (point - 1 >= 0 && !visited[point - 1]) { // 한칸 -1로 갔을 때 범위가 0이상이면 queue에 시간 1 늘려서 저장
+            if (point >= 1 && !visited[point - 1]) { // 한칸 -1로 갔을 때 범위가 0이상이면 queue에 시간 1 늘려서 저장
                 visited[point - 1] = true;
                 queue.offer(new int[]{point - 1, time + 1});
             }
-            if (point + 1 <= 100_000 && !visited[point + 1]) { //한칸 +1로 갔을 때 100_000 이하이면 queue에 시간 1 줄여서 저장
+            if (point < 100_000 && !visited[point + 1]) { //한칸 +1로 갔을 때 100_000 이하이면 queue에 시간 1 줄여서 저장
                 visited[point + 1] = true;
                 queue.offer(new int[]{point + 1, time + 1});
             }
         }
-        return -1;
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 수빈이
-        int K = Integer.parseInt(st.nextToken()); // 동생
-
-
-        System.out.println(bfs(N, K));
 
 
     }
