76762fa7dc2aad00ee8a8f62247bbca4bfffdc57
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-04-19 15:16:01 +0900

    [level 2] Title: 프로세스, Time: 0.94 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,57 +1,43 @@
-// 3시 42분 시작 
-// cdab? 그럼 숫자가 같으면 뒤에부터 우선순위를 가지는거야?
-// 아니 우선순위 다음부터 시작하네
-
-// 그럼 queue를 만들어서 앞에서 부터 뺀다고 생각해보자
-// 2132 이라고 하면 2 빼고 1보다 2가 높으니까 
-// 2  // 2 1 // 3 -- 2 1 // 3 2 -- 2 1 // 3 2 2 // 3 2 2 1
-// 1232라고한다면
-// 1 // 2 -- 1 // 3 -- 1 2 // 3 2 -- 1 2// 3 2 1 -- 2 // 3 2 2 -- 1 // 3 2 2 1
-
-// queue 를 두개 만들어야한다. process될 큐랑 우선순위를 위해 다시 보류하는 큐
-// 그렇다면 location에 해당하는 곳인지 어떻게 알 수 있지?
-//  1 2 3 4 ... 인 로케이션을 위한 배열을 만들고 같이 움직여준다면?
-
 import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.ArrayList;
 
 class Solution {
-    public Queue<Integer> queue;
-    public List<Integer> list;
     public int solution(int[] priorities, int location) {
+        
+        // 큐에 몇번째인지와 숫자 저장
+        Queue<int[]> queue = new LinkedList<>();
+        
+        //우선순위큐에 모든 숫자를 넣고 내림차순으로 저장
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        // 숫자가 클 수록 우선순위 높음
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(new int[]{i, priorities[i]}); // 0 : 몇번째인지 1 : 숫자
+            pq.offer(priorities[i]); 
+        }
+
         int answer = 0;
-        queue = new LinkedList<>();
-        list = new ArrayList<>();
-        
-        for(int i=0;i<priorities.length;i++){
-            queue.offer(i);
-        }
-        // queue 0,1,2,3,4,5
-        
-        for(int i=0;i<priorities.length;i++){
-            int max = 0;
-            int max_seq = 0;
-            
-            for(int x : queue){
-                if(priorities[x]>max){
-                    max = priorities[x];
-                    max_seq = x;
-                }
+
+        while (!queue.isEmpty()) {
+            if (pq.peek() != queue.peek()[1]) { // 만약 가장 큰수가 현재 맨 앞에 있지 않으면
+                queue.offer(queue.poll()); // 빼고 순서 맨 뒤에 다시 넣는다.
+                continue;
             }
-            while(queue.peek()!=max_seq){
-                queue.offer(queue.poll());
-            }
-            list.add(queue.poll());
-        }
-        System.out.println(list);
-        for(int i =0;i<list.size();i++){
-            if(location == list.get(i)){
-                answer = i+1;
+            // 만약 가장 큰 수가 현재 맨앞에 있다면
+            if (queue.peek()[0] == location) { // 동시에 원하는 순번이라면
+                answer++; // 이제까지 빠져나간 횟수에 하나 더하고 break;
                 break;
             }
+            
+            //원하는 순번은 아니면 queue의 맨앞 제거, pq의 맨앞 제거
+            queue.poll();
+            pq.poll();
+            answer++;
         }
+
         return answer;
+
+
     }
 }
