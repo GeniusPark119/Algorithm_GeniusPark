f872cd3d3e4a00a64a631246920b3db6854fa512
SoJung Park <99484238+GeniusPark119@users.noreply.github.com>
2024-05-30 01:13:58 +0900

    [Gold V] Title: 내일 할거야, Time: 2544 ms, Memory: 260980 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -10,29 +10,29 @@ public class Main { // 7983 내일 할거야
 
         int n = Integer.parseInt(br.readLine());
 
-        int[][] homework = new int[n][2];
+        int[][] homework = new int[n][2]; // n개의 과제의 { 걸리는 일 d, 기한 t } 저장
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             homework[i][0] = Integer.parseInt(st.nextToken());
             homework[i][1] = Integer.parseInt(st.nextToken());
         }
-
+        // greedy : 기한이 가장 긴 과제부터 터는것이 가장 최적
         Arrays.sort(homework, (o1, o2) -> o2[1] - o1[1]);
 
-        int day = 1000000001;
+        int day = 1000000001; // 최댓값
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             int d = homework[i][0];
             int t = homework[i][1];
 
-            while(d-->0){
-                if(day<=t){
-                    day--;
-                    t--;
-                }else{
-                    day = t-1;
-                    t--;
+            while (d-- > 0) { // 걸리는 일을 털자!
+                if (day <= t) { // 만약 날짜가 t와 같거나 작으면
+                    day--; // 날짜도 깎고
+                    t--; // 기한도 깎고
+                } else { // 만약 날짜가 더 뒤에 있으면 가져와야함
+                    day = t - 1; // 가져와서 깎기
+                    t--; // 기한도 깎고
                 }
             }
         }
